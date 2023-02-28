5636640002f242c59e1daa8d923d8712099980d9
Suhas Aggarwal <suhasagg@gmail.com>
2023-02-28 18:35:22 +0530

    Create mempooltransactionhandler.go

---
+++ mempooltransactionhandler.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+    "fmt"
+)
+
+type Transaction struct {
+    ID      int
+    Amount  float64
+}
+
+func main() {
+    mempool := make(chan Transaction, 10)
+
+    // start a goroutine to process transactions
+    go func() {
+        for {
+            tx := <-mempool // read from the channel
+            fmt.Printf("Processing transaction %d for amount %.2f\n", tx.ID, tx.Amount)
+            // process the transaction
+        }
+    }()
+
+    // add some transactions to the mempool
+    for i := 1; i <= 5; i++ {
+        tx := Transaction{ID: i, Amount: float64(i * 100)}
+        mempool <- tx // send to the channel
+    }
+
+    // wait for the transactions to be processed
+    for i := 1; i <= 5; i++ {
+        <-mempool // read from the channel to block until processing is done
+    }
+}
